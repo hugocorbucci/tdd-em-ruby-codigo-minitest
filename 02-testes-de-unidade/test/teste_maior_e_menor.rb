2b4a7184eebf812edaac78ff9f644fc51af00096
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 12:57:14 -0900

    Teste que falha na seção 2.3

---
+++ 02-testes-de-unidade/test/teste_maior_e_menor.rb
@@ -0,0 +1,21 @@
+#encoding: UTF-8
+require 'minitest/autorun'
+require_relative '../lib/maior_e_menor.rb'
+require_relative '../lib/carrinho_de_compras.rb'
+require_relative '../lib/produto.rb'
+
+class TesteMaiorMenor < MiniTest::Unit::TestCase
+  def test_ordem_descrescente
+    carrinho = CarrinhoDeCompras.new
+
+    carrinho << Produto.new('Geladeira', 450.0)
+    carrinho << Produto.new('Liquidificador', 250.0)
+    carrinho << Produto.new('Jogo de pratos', 70.0)
+
+    algoritmo = MaiorEMenor.new
+    algoritmo.encontra carrinho
+    
+    assert_equal 'Jogo de pratos', algoritmo.menor.nome
+    assert_equal 'Geladeira', algoritmo.maior.nome
+  end
+end
