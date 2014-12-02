8e8b16fc52da3bd2118dfd9f0e011306b406af43
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 13:20:40 -0900

    Primeiro teste para o capítulo 4

---
+++ 04-baby-steps/test/calculadora_de_salario_test.rb
@@ -0,0 +1,15 @@
+#encoding: UTF-8
+require 'minitest/autorun'
+require_relative '../lib/calculadora_de_salario.rb'
+require_relative '../lib/funcionario.rb'
+
+class CalculadoraDeSalarioTest < MiniTest::Unit::TestCase
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_abaixo_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 1500.0, Cargo::DESENVOLVEDOR)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 1500 * 0.9, salario
+  end
+end
