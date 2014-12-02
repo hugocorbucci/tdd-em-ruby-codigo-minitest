4e04b8f86dfd84d174925665fbbc7c5776b31069
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 13:38:37 -0900

    Adicionando teste falhando para 1 item no carrinho

--- 05-tdd-e-design/test/teste_maior_preco.rb
+++ 05-tdd-e-design/test/teste_maior_preco.rb
@@ -13,4 +13,13 @@ class TesteMaiorPreco < MiniTest::Unit::TestCase
 
     assert_equal 0, valor
   end
+  def teste_deve_retornar_valor_do_item_se_carrinho_com_1_elemento
+    carrinho = CarrinhoDeCompras.new
+    carrinho << Item.new('Geladeira', 1, 900.0)
+
+    algoritmo = MaiorPreco.new
+    valor = algoritmo.encontra carrinho
+
+    assert_equal 900.0, valor
+  end
 end
