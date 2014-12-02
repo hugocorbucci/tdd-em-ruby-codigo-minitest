6fcda93de8db1b37d172bb686f97ec3cb4921a35
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 13:11:30 -0900

    Adicionando teste para II

--- 03-tdd/test/conversor_de_numero_roman_test.rb
+++ 03-tdd/test/conversor_de_numero_roman_test.rb
@@ -14,4 +14,10 @@ class ConversorDeNumeroRomanoTest < MiniTest::Unit::TestCase
     numero = romano.converte 'V'
     assert_equal 5, numero
   end
+
+  def teste_deve_entender_dois_simbolos_como_II
+    romano = ConversorDeNumeroRomano.new
+    numero = romano.converte 'II'
+    assert_equal 2, numero
+  end
 end
