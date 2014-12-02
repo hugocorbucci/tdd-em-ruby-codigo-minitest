17cfbfa425963248860fa0d91833d0687621f33f
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 13:22:25 -0900

    Teste que falha para salário acima do limite

--- 04-baby-steps/test/calculadora_de_salario_test.rb
+++ 04-baby-steps/test/calculadora_de_salario_test.rb
@@ -12,4 +12,12 @@ class CalculadoraDeSalarioTest < MiniTest::Unit::TestCase
 
     assert_equal 1500 * 0.9, salario
   end
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_acima_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 4000.0, Cargo::DESENVOLVEDOR)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 4000 * 0.8, salario
+  end
 end
