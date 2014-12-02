f8a0f67f333d83491f0d253a91d0dac904c7f475
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-02 13:12:42 -0900

    Refatorando para usar acumulador e iteração

--- 03-tdd/lib/conversor_de_numero_romano.rb
+++ 03-tdd/lib/conversor_de_numero_romano.rb
@@ -2,7 +2,6 @@
 class ConversorDeNumeroRomano
   TABELA = {
     'I' => 1,
-    'II'=> 2,
     'V' => 5,
     'X' => 10,
     'L' => 50,
@@ -11,6 +10,10 @@ class ConversorDeNumeroRomano
     'M' => 1000,
   }
   def converte numero_em_romano
-    TABELA[numero_em_romano]
+    acumulador = 0
+    numero_em_romano.split('').each do |digito_em_romano|
+      acumulador += TABELA[digito_em_romano]
+    end
+    acumulador
   end
 end
