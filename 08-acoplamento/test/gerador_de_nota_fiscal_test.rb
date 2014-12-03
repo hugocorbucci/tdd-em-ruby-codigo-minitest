6d01da1aacc5c7b1857145be8b89e81db3cab753
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 10:27:30 -0900

    Passando o teste refatorando para receber o dao no construtor

--- 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
+++ 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
@@ -11,7 +11,7 @@ class GeradorDeNotaFiscalTest < MiniTest::Unit::TestCase
     # marcando o que esperamos que aconteça
     dao.expects(:persiste)
 
-    gerador = GeradorDeNotaFiscal.new
+    gerador = GeradorDeNotaFiscal.new dao
     pedido = Pedido.new('Mauricio', 1000, 1)
 
     nf = gerador.gera pedido
