955f93e16f266f25c4cb5e60cb84e07a808812b6
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 11:11:42 -0900

    Primeira versão dos testes do botao

---
+++ 11-testes-de-codigo-dinamico/test/teste_botao.rb
@@ -0,0 +1,21 @@
+#encoding: UTF-8
+require 'minitest/autorun'
+require 'mocha/setup'
+require_relative '../lib/botao.rb'
+
+class TesteBotao < MiniTest::Unit::TestCase
+  def teste_chama_metodo_no_alvo_ao_executar
+    botao = Botao.new(:teste)
+    alvo = mock
+    alvo.expects(:teste).with()
+
+    botao.executa_em(alvo)
+  end
+  def teste_chama_metodo_no_alvo_com_parametros_ao_executar
+    botao = Botao.new(:teste, :parametro1, [:parametro2])
+    alvo = mock
+    alvo.expects(:teste).with(:parametro1, [:parametro2])
+
+    botao.executa_em(alvo)
+  end
+end
