0de101584023f3b3bdba7dc03e6f4cc6098b9cc3
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 09:50:49 -0900

    Implementação de base para capítulo 7

---
+++ 07-coesao/test/calculadora_de_salario_test.rb
@@ -0,0 +1,23 @@
+#encoding: UTF-8
+require 'minitest/autorun'
+require_relative '../lib/calculadora_de_salario.rb'
+require_relative '../lib/funcionario.rb'
+
+class CalculadoraDeSalarioTest < MiniTest::Unit::TestCase
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_abaixo_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 1500.0, Cargo::DESENVOLVEDOR)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 1500 * 0.9, salario
+  end
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_acima_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 4000.0, Cargo::DESENVOLVEDOR)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 4000 * 0.8, salario
+  end
+end
