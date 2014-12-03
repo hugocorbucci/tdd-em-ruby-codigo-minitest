f4036c6a86824fde16d06c04d3b7e07174bffaf9
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 09:53:16 -0900

    Adicionando testes para DBAs

--- 07-coesao/test/calculadora_de_salario_test.rb
+++ 07-coesao/test/calculadora_de_salario_test.rb
@@ -20,4 +20,20 @@ class CalculadoraDeSalarioTest < MiniTest::Unit::TestCase
 
     assert_equal 4000 * 0.8, salario
   end
+  def teste_deve_calcular_salario_para_DBAs_com_salario_abaixo_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 1500.0, Cargo::DBA)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 1500 * 0.85, salario
+  end
+  def teste_deve_calcular_salario_para_DBAs_com_salario_acima_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 4500.0, Cargo::DBA)
+
+    salario = calculadora.calcula_salario desenvolvedor
+
+    assert_equal 4500 * 0.75, salario
+  end
 end
