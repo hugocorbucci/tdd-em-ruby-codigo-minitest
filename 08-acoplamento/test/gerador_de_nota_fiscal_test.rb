f14f0bb22b9f769f6847ad32acbfb2019eec1f7a
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 10:52:12 -0900

    Adicionando teste que requer usar a tabela

--- 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
+++ 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
@@ -14,4 +14,15 @@ class GeradorDeNotaFiscalTest < MiniTest::Unit::TestCase
 
     nf = gerador.gera pedido
   end
+  def teste_deve_consultar_a_tabela_para_calcular_valor
+    tabela = mock
+    tabela.expects(:[]).with(1000.0).returns(0.2)
+
+    gerador = GeradorDeNotaFiscal.new [], tabela
+    pedido = Pedido.new('Mauricio', 1000, 1)
+
+    nf = gerador.gera pedido
+
+    assert_equal 1000 * 0.2, nf.valor
+  end
 end
