38822a192b20bed3a217999443194a29e5bcbde7
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 09:39:37 -0900

    Melhorando testes para calculadora com classes de equivalencia

--- 06-cuidando-dos-testes/test/calculadora_test.rb
+++ 06-cuidando-dos-testes/test/calculadora_test.rb
@@ -3,13 +3,20 @@ require 'minitest/autorun'
 require_relative '../lib/calculadora.rb'
 
 class CalculadoraTest < MiniTest::Unit::TestCase
-  def teste_deve_somar_um_mais_um
-    assert_equal 2, Calculadora.new.soma(1,1)
-  end
-  def teste_deve_somar_um_mais_dois
-    assert_equal 3, Calculadora.new.soma(1,2)
-  end
-  def teste_deve_somar_dois_mais_dois
+  def teste_deve_somar_dois_numeros_positivos
     assert_equal 4, Calculadora.new.soma(2,2)
+  end
+  def teste_deve_somar_positivo_com_negativo
+    assert_equal 4, Calculadora.new.soma(6,-2)
+  end
+  def teste_deve_somar_negativo_com_positivo
+    assert_equal -4, Calculadora.new.soma(-6,2)
+  end
+  def teste_deve_somar_dois_numeros_negativos
+    assert_equal -4, Calculadora.new.soma(-2,-2)
+  end
+  def teste_deve_somar_com_zero
+    assert_equal 4, Calculadora.new.soma(0,4)
+    assert_equal 4, Calculadora.new.soma(4,0)
   end
 end
