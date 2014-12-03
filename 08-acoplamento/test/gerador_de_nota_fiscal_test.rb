e2e4040725fbb7808c8525f7c2f18725a4c8fe28
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 10:48:01 -0900

    Refatorando para receber acoes genéricas no gerador

--- 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
+++ 08-acoplamento/test/gerador_de_nota_fiscal_test.rb
@@ -5,21 +5,11 @@ require_relative '../lib/pedido.rb'
 require_relative '../lib/gerador_de_nota_fiscal.rb'
 
 class GeradorDeNotaFiscalTest < MiniTest::Unit::TestCase
-  def teste_deve_persistir_NF_gerada
-    dao = mock
-    dao.expects(:persiste)
-    sap = stub_everything
-    
-    gerador = GeradorDeNotaFiscal.new dao, sap
-    pedido = Pedido.new('Mauricio', 1000, 1)
+  def teste_deve_invocar_acoes_posteriores
+    acao1 = mock.tap{|dao| dao.expects(:executa)}
+    acao2 = mock.tap{|sap| sap.expects(:executa)}
 
-    nf = gerador.gera pedido
-  end
-  def teste_deve_enviar_NF_gerada_para_SAP
-    dao = stub_everything
-    sap = mock.tap{|sap| sap.expects(:envia)}
-
-    gerador = GeradorDeNotaFiscal.new dao, sap
+    gerador = GeradorDeNotaFiscal.new [acao1, acao2]
     pedido = Pedido.new('Mauricio', 1000, 1)
 
     nf = gerador.gera pedido
